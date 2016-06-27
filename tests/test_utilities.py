1a176b4dc5c8b6034963b9daa0579d898884b26e
Christopher C Strelioff <chris.strelioff@gmail.com>
2016-06-27 11:32:09 -0700

    test_utilities.py -- migrated to py.test

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -17,7 +17,7 @@ from future.builtins import (ascii, bytes, chr, dict, filter, hex,  # noqa
                              input, int, map, next, oct, open, pow, range,
                              round, str, super, zip)
 
-import unittest
+import pytest
 
 from scipy.stats import beta
 
@@ -25,48 +25,38 @@ from conjugate import central_credible_region
 from conjugate import high_density_credible_region
 
 
-class UtilitiesTest(unittest.TestCase):
-    """Test the utilities methods."""
+def test_hdcr_binomial_01():
+    """
+    * utils: test_hdcr_binomial_01 -- test HDCR for binomial inference 01
+    """
+    hdcr = list(high_density_credible_region(beta(5, 5)))
+    pre_comp = [0.212008506778868, 0.787991493221132]
 
-    def setUp(self):
-        """setUp -- utilities tests."""
-        pass
+    assert hdcr == pre_comp
 
-    def tearDown(self):
-        pass
+def test_hdcr_binomial_02():
+    """
+    * utils: test_hdcr_binomial_02 -- test HDCR for binomial inference 02
+    """
+    hdcr = list(high_density_credible_region(beta(1, 10)))
+    pre_comp = [0.0, 0.25886555089305224]
 
-    def test_hdcr_binomial_01(self):
-        """
-        * utils: test_hdcr_binomial_01 -- test HDCR for binomial inference 01
-        """
-        hdcr = list(high_density_credible_region(beta(5, 5)))
-        pre_comp = [0.212008506778868, 0.787991493221132]
+    assert hdcr == pre_comp
 
-        self.assertListEqual(hdcr, pre_comp)
+def test_ccr_binomial_01():
+    """
+    * utils: test_ccr_binomial_01 -- test CCR for binomial inference 01
+    """
+    ccr = list(central_credible_region(beta(5, 5)))
+    pre_comp = [0.212008506778868, 0.787991493221132]
 
-    def test_hdcr_binomial_02(self):
-        """
-        * utils: test_hdcr_binomial_02 -- test HDCR for binomial inference 02
-        """
-        hdcr = list(high_density_credible_region(beta(1, 10)))
-        pre_comp = [0.0, 0.25886555089305224]
+    assert ccr, pre_comp
 
-        self.assertListEqual(hdcr, pre_comp)
+def test_ccr_binomial_02():
+    """
+    * utils: test_ccr_binomial_02 -- test CCR for binomial inference 02
+    """
+    ccr = list(central_credible_region(beta(1, 10)))
+    pre_comp = [0.0025285785444617869, 0.30849710781876077]
 
-    def test_ccr_binomial_01(self):
-        """
-        * utils: test_ccr_binomial_01 -- test CCR for binomial inference 01
-        """
-        ccr = list(central_credible_region(beta(5, 5)))
-        pre_comp = [0.212008506778868, 0.787991493221132]
-
-        self.assertListEqual(ccr, pre_comp)
-
-    def test_ccr_binomial_02(self):
-        """
-        * utils: test_ccr_binomial_02 -- test CCR for binomial inference 02
-        """
-        ccr = list(central_credible_region(beta(1, 10)))
-        pre_comp = [0.0025285785444617869, 0.30849710781876077]
-
-        self.assertListEqual(ccr, pre_comp)
+    assert ccr == pre_comp
