ec89ed3dd9c747351c54e808b4ae71626e5804c3
Christopher C Strelioff <chris.strelioff@gmail.com>
2016-06-27 11:08:59 -0700

    test_multinomial.py -- migrated to py.test

--- tests/test_multinomial.py
+++ tests/test_multinomial.py
@@ -17,7 +17,7 @@ from future.builtins import (ascii, bytes, chr, dict, filter, hex,  # noqa
                              input, int, map, next, oct, open, pow, range,
                              round, str, super, zip)
 
-import unittest
+import pytest
 
 from conjugate import PosteriorBase
 from conjugate import MultinomialDirichlet
@@ -25,257 +25,328 @@ from conjugate import ConjugateDataException
 from conjugate import ConjugateParameterException
 
 
-class MultinomialDirichletTest(unittest.TestCase):
-    """Test the MultinomialDirichlet class."""
+@pytest.fixture
+def setup():
+    alphabet = [str('a'), str('b'), str('c'), str('d')]
+    multinomp = MultinomialDirichlet(alphabet)
+    return {'alphabet': alphabet, 'multinomp': multinomp}
 
-    def setUp(self):
-        self.alphabet = [str('a'), str('b'), str('c'), str('d')]
-        self.multinomp = MultinomialDirichlet(self.alphabet)
 
-    def tearDown(self):
-        del self.alphabet
-        del self.multinomp
+def test_instantiate(setup):
+    """
+    * multinomial: test_instantiate -- instantiate and test is instance
+    of PosteriorBase ABC.
+    """
+    multinomp = setup['multinomp']
 
-    def test_instantiate(self):
-        """
-        * multinomial: test_instantiate -- instantiate and test is instance
-        of PosteriorBase ABC.
-        """
-        self.assertIsInstance(self.multinomp, PosteriorBase)
+    assert isinstance(multinomp, PosteriorBase)
 
-    def test_contains(self):
-        """
-        * mulitnomial: test_contains -- make sure p in self works...
-        """
-        self.assertTrue('p_a' in self.multinomp)
 
-    def test_contains_invalid(self):
-        """
-        * mulitnomial: test_contains_invalid -- try invalid parameter...
-        """
-        self.assertFalse('p_nonsense' in self.multinomp)
+def test_contains(setup):
+    """
+    * mulitnomial: test_contains -- make sure p in self works...
+    """
+    multinomp = setup['multinomp']
 
-    def test_iter(self):
-        """
-        * multinomial: test_iter -- test the __iter__ method...
-        """
-        params = []
-        for item in self.multinomp:
-            params.append(item)
+    assert 'p_a' in multinomp
 
-        self.assertListEqual(params, ['p_a', 'p_b', 'p_c', 'p_d'])
 
-    def test_str(self):
-        """
-        * multinomial: test_str -- does __str__ method work?, yup...
-        """
-        data = {str('a'): 10, str('b'): 0, str('c'): 5, str('d'): 2}
-        self.multinomp.data = data
-        prior = self.multinomp.prior_hyperparameters
-        tmp = ('mp = MultinomialDirichlet({})\n'
-               'mp.data = {}\n'
-               'mp.prior_hyperparameters = {}'.format(self.alphabet,
-                                                      data,
-                                                      prior))
-        self.assertTrue(tmp == str(self.multinomp))
+def test_contains_invalid(setup):
+    """
+    * mulitnomial: test_contains_invalid -- try invalid parameter...
+    """
+    multinomp = setup['multinomp']
 
-    def test_data_setter_valid01(self):
-        """
-        * multinomial: test_data_setter_valid01 -- use the data setter with
-        valid, simple input
-        """
-        data = {'a': 10, 'b': 0, 'c': 5, 'd': 2}
-        self.multinomp.data = data
+    assert 'p_nonsense' not in multinomp
 
-        self.assertDictEqual(data, self.multinomp.data)
 
-    def test_data_setter_valid02(self):
-        """
-        * multinomial: test_data_setter_valid02 -- use the data setter with
-        valid input; leave off one key-value pair
-        """
-        data = {'a': 10, 'b': 0, 'd': 2}
-        self.multinomp.data = data
+def test_iter(setup):
+    """
+    * multinomial: test_iter -- test the __iter__ method...
+    """
+    multinomp = setup['multinomp']
 
-        cnta = (self.multinomp.data['a'] == 10)
-        cntb = (self.multinomp.data['b'] == 0)
-        cntc = (self.multinomp.data['c'] == 0)  # this should b okay!
-        cntd = (self.multinomp.data['d'] == 2)
-        self.assertTrue(cnta and cntb and cntc and cntd)
+    params = []
+    for item in multinomp:
+        params.append(item)
 
-    def test_data_setter_invalid01(self):
-        """
-        * multinomial: test_data_setter_invalid01 -- use the data setter with
-        invalid input; bad key.
-        """
-        data = {'a': 10, 'b': 0, 'c': 5, 'g': 2}
+    assert params == ['p_a', 'p_b', 'p_c', 'p_d']
 
-        with self.assertRaises(ConjugateDataException):
-            self.multinomp.data = data
 
-    def test_data_setter_invalid02(self):
-        """
-        * multinomial: test_data_setter_invalid02 -- use the data setter with
-        invalid input; negative value.
-        """
-        data = {'a': 10, 'b': 0, 'c': -2}
+def test_str(setup):
+    """
+    * multinomial: test_str -- does __str__ method work?, yup...
+    """
+    alphabet = setup['alphabet']
+    multinomp = setup['multinomp']
 
-        with self.assertRaises(ConjugateDataException):
-            self.multinomp.data = data
+    data = {str('a'): 10, str('b'): 0, str('c'): 5, str('d'): 2}
+    multinomp.data = data
+    prior = multinomp.prior_hyperparameters
+    tmp = ('mp = MultinomialDirichlet({})\n'
+           'mp.data = {}\n'
+           'mp.prior_hyperparameters = {}'.format(alphabet,
+                                                  data,
+                                                  prior))
+    assert tmp == str(multinomp)
 
-    def test_data_setter_invalid03(self):
-        """
-        * multinomial: test_data_setter_invalid03 -- use the data setter with
-        invalid input; data as list.
-        """
-        data = [10, 0, 2]
 
-        with self.assertRaises(ConjugateDataException):
-            self.multinomp.data = data
+def test_data_setter_valid01(setup):
+    """
+    * multinomial: test_data_setter_valid01 -- use the data setter with
+    valid, simple input
+    """
+    multinomp = setup['multinomp']
 
-    def test_distribution(self):
-        """
-        * multinomial: test_distribution -- returns str with dist name...
-        """
-        self.assertTrue(self.multinomp.distribution ==
-                        'Distribution: Multinomial')
+    data = {'a': 10, 'b': 0, 'c': 5, 'd': 2}
+    multinomp.data = data
 
-    def test_distribution_parameter_names_valid(self):
-        """
-        * multinomial: test_distribution_parameter_names_valid -- basic test
-        of returned list...
-        """
-        self.assertListEqual(self.multinomp.distribution_parameter_names,
-                             ['p_a', 'p_b', 'p_c', 'p_d'])
+    assert data == multinomp.data
 
-    def test_distribution_parameter_names_invalid(self):
-        """
-        * multinomial: test_distribution_parameter_names_invalid -- try to
-        set the parameter names; can't, they're read-only...
-        """
-        with self.assertRaises(AttributeError):
-            self.multinomp.distribution_parameter_names = ['p_one', 'p_two']
 
-    def test_distribution_parameter_supoort(self):
-        """
-        * multinomial: test_distribution_parameter_support -- make sure all
-        supports are (0.0, 1.0)...
-        """
-        sups = []
-        for p in self.multinomp:
-            sups.append(self.multinomp.distribution_parameter_support[p] ==
-                        (0.0, 1.0))
+def test_data_setter_valid02(setup):
+    """
+    * multinomial: test_data_setter_valid02 -- use the data setter with
+    valid input; leave off one key-value pair
+    """
+    multinomp = setup['multinomp']
 
-        self.assertTrue(sups.count(True) == 4)
+    data = {'a': 10, 'b': 0, 'd': 2}
+    multinomp.data = data
 
-    def test_prior(self):
-        """
-        * multinomial: test_prior -- returns str with prior name...
-        """
-        self.assertTrue(self.multinomp.prior ==
-                        'Prior: Dirichlet')
+    cnta = (multinomp.data['a'] == 10)
+    cntb = (multinomp.data['b'] == 0)
+    cntc = (multinomp.data['c'] == 0)  # this should b okay!
+    cntd = (multinomp.data['d'] == 2)
 
-    def test_prior_hyperparameter_names(self):
-        """
-        * multinomial: test_prior_hyperparameter_names -- returns list with
-        prior hyperparameter name...
-        """
-        self.assertListEqual(self.multinomp.prior_hyperparameter_names,
-                             ['a_a', 'a_b', 'a_c', 'a_d'])
+    assert (cnta and cntb and cntc and cntd)
 
-    def test_prior_hyperparameter_names_assign(self):
-        """
-        * multinomial: test_prior_hyperparameter_names_assign -- try to assign
-        prior hyperparameter names -- not allowed...
-        """
-        with self.assertRaises(AttributeError):
-            self.multinomp.prior_hyperparameter_names = ['a_fred', 'a_ted']
 
-    def test_prior_hyperparameters(self):
-        """
-        * multinomial: test_prior_hyperparameters -- returns dict with
-        prior hyperparameter names and values...
-        """
-        hp = {'a_{}'.format(l): 1 for l in self.alphabet}
+def test_data_setter_invalid01(setup):
+    """
+    * multinomial: test_data_setter_invalid01 -- use the data setter with
+    invalid input; bad key.
+    """
+    multinomp = setup['multinomp']
 
-        self.assertDictEqual(self.multinomp.prior_hyperparameters, hp)
+    data = {'a': 10, 'b': 0, 'c': 5, 'g': 2}
 
-    def test_prior_hyperparameters_assign_valid01(self):
-        """
-        * multinomial: test_prior_hyperparameters_assign_valid01 -- assign
-        prior hyperparameter with valid dict...
-        """
-        hp = {'a_{}'.format(l): 5 for l in self.alphabet}
-        self.multinomp.prior_hyperparameters = hp
+    with pytest.raises(ConjugateDataException):
+        multinomp.data = data
 
-        self.assertDictEqual(self.multinomp.prior_hyperparameters, hp)
 
-    def test_prior_hyperparameters_assign_valid02(self):
-        """
-        * multinomial: test_prior_hyperparameters_assign_valid02 -- assign
-        prior hyperparameter using subset of hyperparameters...
-        """
-        hp = {'a_a': 10, 'a_c': 2}
-        self.multinomp.prior_hyperparameters = hp
+def test_data_setter_invalid02(setup):
+    """
+    * multinomial: test_data_setter_invalid02 -- use the data setter with
+    invalid input; negative value.
+    """
+    multinomp = setup['multinomp']
 
-    def test_prior_hyperparameters_assign_invalid01(self):
-        """
-        * multinomial: test_prior_hyperparameters_assign_invalid01 -- assign
-        prior hyperparameter with invalid dict...
-        """
-        # incorrect hyperparameter names
-        hp = {'param_{}'.format(l): 5 for l in self.alphabet}
+    data = {'a': 10, 'b': 0, 'c': -2}
 
-        with self.assertRaises(ConjugateParameterException):
-            self.multinomp.prior_hyperparameters = hp
+    with pytest.raises(ConjugateDataException):
+        multinomp.data = data
 
-    def test_prior_hyperparameters_assign_invalid02(self):
-        """
-        * multinomial: test_prior_hyperparameters_assign_invalid02 -- assign
-        prior hyperparameter with invalid dict...
-        """
-        with self.assertRaises(ConjugateParameterException):
-            # negative value
-            self.multinomp.prior_hyperparameters = {'a_a': 10, 'a_b': -3}
 
-    def test_prior_hyperparameters_assign_invalid03(self):
-        """
-        * multinomial: test_prior_hyperparameters_assign_invalid03 -- assign
-        prior hyperparameter with list-- not allowed...
-        """
-        with self.assertRaises(ConjugateParameterException):
-            # try to use list -- doesn'tmake sense
-            self.multinomp.prior_hyperparameters = [10, 2, 5, 8]
+def test_data_setter_invalid03(setup):
+    """
+    * multinomial: test_data_setter_invalid03 -- use the data setter with
+    invalid input; data as list.
+    """
+    multinomp = setup['multinomp']
 
-    def test_prior_mean_invalid01(self):
-        """
-        * multinomial: test_prior_mean_invalid01 -- try to access prior mean
-        for parameter that doesn't make sense...
-        """
-        with self.assertRaises(ConjugateParameterException):
-            self.multinomp.prior_mean('p_nonsense')
+    data = [10, 0, 2]
 
-    def test_prior_mean_valid01(self):
-        """
-        * multinomial: test_prior_mean_valid01 -- simple test of prior mean...
-        """
-        pa = (self.multinomp.prior_mean('p_a') == 1/4)
-        pb = (self.multinomp.prior_mean('p_b') == 1/4)
-        pc = (self.multinomp.prior_mean('p_c') == 1/4)
-        pd = (self.multinomp.prior_mean('p_d') == 1/4)
+    with pytest.raises(ConjugateDataException):
+        multinomp.data = data
 
-        self.assertTrue(pa and pb and pc and pd)
 
-    def test_prior_mean_valid02(self):
-        """
-        * multinomial: test_prior_mean_valid02 -- assign prior hyperparameters
-        and do simple test of prior mean...
-        """
-        self.multinomp.prior_hyperparameters = {'a_a': 3}
+def test_distribution(setup):
+    """
+    * multinomial: test_distribution -- returns str with dist name...
+    """
+    multinomp = setup['multinomp']
 
-        pa = (self.multinomp.prior_mean('p_a') == 3/6)
-        pb = (self.multinomp.prior_mean('p_b') == 1/6)
-        pc = (self.multinomp.prior_mean('p_c') == 1/6)
-        pd = (self.multinomp.prior_mean('p_d') == 1/6)
+    assert multinomp.distribution == 'Distribution: Multinomial'
 
-        self.assertTrue(pa and pb and pc and pd)
+
+def test_distribution_parameter_names_valid(setup):
+    """
+    * multinomial: test_distribution_parameter_names_valid -- basic test
+    of returned list...
+    """
+    multinomp = setup['multinomp']
+
+    assert multinomp.distribution_parameter_names == \
+        ['p_a', 'p_b', 'p_c', 'p_d']
+
+
+def test_distribution_parameter_names_invalid(setup):
+    """
+    * multinomial: test_distribution_parameter_names_invalid -- try to
+    set the parameter names; can't, they're read-only...
+    """
+    multinomp = setup['multinomp']
+
+    with pytest.raises(AttributeError):
+        multinomp.distribution_parameter_names = ['p_one', 'p_two']
+
+
+def test_distribution_parameter_support(setup):
+    """
+    * multinomial: test_distribution_parameter_support -- make sure all
+    supports are (0.0, 1.0)...
+    """
+    multinomp = setup['multinomp']
+
+    for p in multinomp:
+        assert multinomp.distribution_parameter_support[p] == (0.0, 1.0)
+
+
+def test_prior(setup):
+    """
+    * multinomial: test_prior -- returns str with prior name...
+    """
+    multinomp = setup['multinomp']
+
+    assert multinomp.prior == 'Prior: Dirichlet'
+
+
+def test_prior_hyperparameter_names(setup):
+    """
+    * multinomial: test_prior_hyperparameter_names -- returns list with
+    prior hyperparameter name...
+    """
+    multinomp = setup['multinomp']
+
+    assert multinomp.prior_hyperparameter_names == ['a_a', 'a_b', 'a_c', 'a_d']
+
+
+def test_prior_hyperparameter_names_assign(setup):
+    """
+    * multinomial: test_prior_hyperparameter_names_assign -- try to assign
+    prior hyperparameter names -- not allowed...
+    """
+    multinomp = setup['multinomp']
+
+    with pytest.raises(AttributeError):
+        multinomp.prior_hyperparameter_names = ['a_fred', 'a_ted']
+
+
+def test_prior_hyperparameters(setup):
+    """
+    * multinomial: test_prior_hyperparameters -- returns dict with
+    prior hyperparameter names and values...
+    """
+    alphabet = setup['alphabet']
+    multinomp = setup['multinomp']
+
+    hp = {'a_{}'.format(l): 1 for l in alphabet}
+
+    assert multinomp.prior_hyperparameters == hp
+
+
+def test_prior_hyperparameters_assign_valid01(setup):
+    """
+    * multinomial: test_prior_hyperparameters_assign_valid01 -- assign
+    prior hyperparameter with valid dict...
+    """
+    alphabet = setup['alphabet']
+    multinomp = setup['multinomp']
+
+    hp = {'a_{}'.format(l): 5 for l in alphabet}
+    multinomp.prior_hyperparameters = hp
+
+    assert multinomp.prior_hyperparameters == hp
+
+
+def test_prior_hyperparameters_assign_valid02(setup):
+    """
+    * multinomial: test_prior_hyperparameters_assign_valid02 -- assign
+    prior hyperparameter using subset of hyperparameters...
+    """
+    alphabet = setup['alphabet']
+    multinomp = setup['multinomp']
+
+    hp_full = {'a_{}'.format(l): 1 for l in alphabet}
+    hp_full['a_a'] = 10
+    hp_full['a_c'] = 2
+    # assign using only a_a and a_c; others default values
+    multinomp.prior_hyperparameters = {'a_a': 10, 'a_c': 2}
+
+    assert multinomp.prior_hyperparameters == hp_full
+
+
+def test_prior_hyperparameters_assign_invalid01(setup):
+    """
+    * multinomial: test_prior_hyperparameters_assign_invalid01 -- assign
+    prior hyperparameter with invalid dict...
+    """
+    alphabet = setup['alphabet']
+    multinomp = setup['multinomp']
+
+    # incorrect hyperparameter names
+    hp = {'param_{}'.format(l): 5 for l in alphabet}
+
+    with pytest.raises(ConjugateParameterException):
+        multinomp.prior_hyperparameters = hp
+
+
+def test_prior_hyperparameters_assign_invalid02(setup):
+    """
+    * multinomial: test_prior_hyperparameters_assign_invalid02 -- assign
+    prior hyperparameter with invalid dict...
+    """
+    multinomp = setup['multinomp']
+
+    with pytest.raises(ConjugateParameterException):
+        # negative value
+        multinomp.prior_hyperparameters = {'a_a': 10, 'a_b': -3}
+
+
+def test_prior_hyperparameters_assign_invalid03(setup):
+    """
+    * multinomial: test_prior_hyperparameters_assign_invalid03 -- assign
+    prior hyperparameter with list-- not allowed...
+    """
+    multinomp = setup['multinomp']
+
+    with pytest.raises(ConjugateParameterException):
+        # try to use list -- doesn't make sense
+        multinomp.prior_hyperparameters = [10, 2, 5, 8]
+
+
+def test_prior_mean_invalid01(setup):
+    """
+    * multinomial: test_prior_mean_invalid01 -- try to access prior mean
+    for parameter that doesn't make sense...
+    """
+    multinomp = setup['multinomp']
+
+    with pytest.raises(ConjugateParameterException):
+        multinomp.prior_mean('p_nonsense')
+
+
+def test_prior_mean_valid01(setup):
+    """
+    * multinomial: test_prior_mean_valid01 -- simple test of prior mean...
+    """
+    multinomp = setup['multinomp']
+
+    for p in multinomp:
+        assert multinomp.prior_mean(p) == 1/4
+
+
+def test_prior_mean_valid02(setup):
+    """
+    * multinomial: test_prior_mean_valid02 -- assign prior hyperparameters
+    and do simple test of prior mean...
+    """
+    multinomp = setup['multinomp']
+
+    multinomp.prior_hyperparameters = {'a_a': 3}
+    for p in multinomp:
+        if p == 'p_a':
+            assert multinomp.prior_mean(p) == 3/6
+        else:
+            assert multinomp.prior_mean(p) == 1/6
